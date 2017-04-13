9da4f9d6f4c07154a8f32c29607e9d88ba088ced
Wajahat Karim <wajahatkarim3@gmail.com>
2017-04-13 20:03:03 +0500

    Added basic buttons and image views!

--- app/src/main/java/com/wajahatkarim3/longimagecamera/demo/MainActivity.java
+++ app/src/main/java/com/wajahatkarim3/longimagecamera/demo/MainActivity.java
@@ -1,7 +1,10 @@
 package com.wajahatkarim3.longimagecamera.demo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.wajahatkarim3.longimagecamera.LongImageCameraActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Intent ii = new Intent(this, LongImageCameraActivity.class);
+        startActivity(ii);
     }
 }
---
+++ longimagecamera-lib/src/main/java/com/wajahatkarim3/longimagecamera/LongImageCameraActivity.java
@@ -0,0 +1,15 @@
+package com.wajahatkarim3.longimagecamera;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LongImageCameraActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_long_image_camera);
+
+
+    }
+}
