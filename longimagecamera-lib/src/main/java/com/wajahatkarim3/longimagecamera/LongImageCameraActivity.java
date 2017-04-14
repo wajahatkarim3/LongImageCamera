5cd9b78156b5cc03684ef4e375ac1e626e0f80d7
Wajahat Karim <wajahatkarim3@gmail.com>
2017-04-14 14:07:03 +0500

    Added CameraView and permissions handling for camera....

--- longimagecamera-lib/src/main/java/com/wajahatkarim3/longimagecamera/LongImageCameraActivity.java
+++ longimagecamera-lib/src/main/java/com/wajahatkarim3/longimagecamera/LongImageCameraActivity.java
@@ -1,20 +1,35 @@
 package com.wajahatkarim3.longimagecamera;
 
+import android.Manifest;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.graphics.SurfaceTexture;
+import android.hardware.camera2.CameraManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.TextureView;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.ImageView;
 
+import com.google.android.cameraview.CameraView;
+
+import java.security.Permission;
+
 public class LongImageCameraActivity extends AppCompatActivity {
 
+    private static final int MY_PERMISSIONS_REQUEST_CAMERA = 1002;
     public final String TAG = LongImageCameraActivity.class.getSimpleName();
 
     Button btnSnap, btnDone;
     ImageView imgRecent;
+    CameraView cameraView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +39,9 @@ public class LongImageCameraActivity extends AppCompatActivity {
         btnSnap = (Button) findViewById(R.id.btnSnap);
         btnDone = (Button) findViewById(R.id.btnDone);
         imgRecent = (ImageView) findViewById(R.id.imgRecent);
+        cameraView = (CameraView) findViewById(R.id.cameraView);
+
+        imgRecent.setVisibility(View.GONE);
 
         btnSnap.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -32,6 +50,25 @@ public class LongImageCameraActivity extends AppCompatActivity {
             }
         });
 
+        checkForCameraPermission();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        int permissionCheck = ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA);
+        if (permissionCheck == PackageManager.PERMISSION_GRANTED)
+        {
+            // Start camera
+            cameraView.start();
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        cameraView.stop();
+        super.onPause();
     }
 
     public void btnSnapClick(View view)
@@ -40,4 +77,97 @@ public class LongImageCameraActivity extends AppCompatActivity {
         Animation animation = AnimationUtils.loadAnimation(this, R.anim.move_up_anim);
         imgRecent.startAnimation(animation);
     }
+
+    public void checkForCameraPermission()
+    {
+        // Assume thisActivity is the current activity
+        int permissionCheck = ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA);
+        if (permissionCheck == PackageManager.PERMISSION_GRANTED)
+        {
+            // Start camera
+            cameraView.start();
+        }
+        else {
+            // Ask for permissions here
+
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.CAMERA)) {
+                // Show dialog explaning why we need camera permission here
+
+                showInfoDialog();
+
+            }
+            else {
+                // No explanation needed, we can request the permission.
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.CAMERA},
+                        MY_PERMISSIONS_REQUEST_CAMERA);
+
+            }
+        }
+    }
+
+    public void showInfoDialog()
+    {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle(getString(R.string.dialog_title));
+        builder.setMessage(getString(R.string.dialog_message));
+
+        String positiveText = getString(android.R.string.ok);
+        builder.setPositiveButton(positiveText,
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        // positive button logic
+
+                        // Ask for permission. User has agreed to grant it.
+                        ActivityCompat.requestPermissions(LongImageCameraActivity.this,
+                                new String[]{Manifest.permission.CAMERA},
+                                MY_PERMISSIONS_REQUEST_CAMERA);
+                    }
+                });
+
+        String negativeText = getString(android.R.string.cancel);
+        builder.setNegativeButton(negativeText,
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        // negative button logic
+
+                        // let it go then
+                    }
+                });
+
+        AlertDialog dialog = builder.create();
+        // display dialog
+        dialog.show();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_CAMERA: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+
+                    cameraView.start();
+
+                } else {
+
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request
+        }
+    }
+
+
 }
